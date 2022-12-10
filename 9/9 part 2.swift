b9b20ec3778b843b6ac3ab5a1c669aaa7cdbcb2d
Wai-Wai Ng <me@waiwaing.com>
2022-12-10 02:27:09 +0000

    Clean up day 9 part 2

--- 9/9 part 2.swift
+++ 9/9 part 2.swift
@@ -4,40 +4,27 @@ let input = try String(decoding: FileHandle(forReadingAtPath: "input.txt")!.read
 
 struct Point: Hashable { let x: Int; let y: Int; }
 var tailVisitedLocations = Set<Point>()
-
 var knots = [Point](repeating: Point(x: 0, y: 0), count: 10)
+tailVisitedLocations.insert(knots[9])
 
 func newKnotLocation(head: Point, tail: Point) -> Point {
     switch (head.x - tail.x, head.y - tail.y) {
+        case (-1...1, -1...1): return tail
         case (+2, +0): return Point(x: tail.x + 1, y: tail.y + 0)
         case (+0, +2): return Point(x: tail.x + 0, y: tail.y + 1)
         case (-2, +0): return Point(x: tail.x - 1, y: tail.y + 0)
         case (+0, -2): return Point(x: tail.x + 0, y: tail.y - 1)
-
-        case (+1 ... +2, +2): return Point(x: tail.x + 1, y: tail.y + 1)
-        case (+2, +1 ... +2): return Point(x: tail.x + 1, y: tail.y + 1)
-        case (+1 ... +2, -2): return Point(x: tail.x + 1, y: tail.y - 1)
-        case (+2, -2 ... -1): return Point(x: tail.x + 1, y: tail.y - 1)
-        case (-2 ... -1, +2): return Point(x: tail.x - 1, y: tail.y + 1)
-        case (-2, +1 ... +2): return Point(x: tail.x - 1, y: tail.y + 1)
-        case (-2 ... -1, -2): return Point(x: tail.x - 1, y: tail.y - 1)
-        case (-2, -2 ... -1): return Point(x: tail.x - 1, y: tail.y - 1)
-        
-        case (-1...1, -1...1): return tail
-        default: 
-            print(head.x - tail.x, head.y - tail.y)
-            print(head.x, tail.x, head.y, tail.y)
-            fatalError()
-        
+        case (+1 ... +2, +1 ... +2): return Point(x: tail.x + 1, y: tail.y + 1)
+        case (+1 ... +2, -2 ... -1): return Point(x: tail.x + 1, y: tail.y - 1)
+        case (-2 ... -1, +1 ... +2): return Point(x: tail.x - 1, y: tail.y + 1)
+        case (-2 ... -1, -2 ... -1): return Point(x: tail.x - 1, y: tail.y - 1)
+        default: fatalError() 
     }
 }
 
-input.split(separator: "\n").flatMap { 
-    let parts = $0.split(separator: " ")
-    return [String](repeating: String(parts[0]), count: Int(parts[1])!)
+input.split(separator: "\n").flatMap { line in 
+    [Character](repeating: line[line.startIndex], count: Int(line.split(separator: " ")[1])!)
 }.forEach { instruction in
-    tailVisitedLocations.insert(knots[9])
-
     switch(instruction) {
         case "U": knots[0] = Point(x: knots[0].x, y: knots[0].y + 1)
         case "D": knots[0] = Point(x: knots[0].x, y: knots[0].y - 1)
@@ -46,10 +33,8 @@ input.split(separator: "\n").flatMap {
         default: fatalError()
     }
 
-    (1...9).forEach { 
-        knots[$0] = newKnotLocation(head: knots[$0 - 1], tail: knots[$0])
-    }
+    (1...9).forEach { i in knots[i] = newKnotLocation(head: knots[i - 1], tail: knots[i])} 
+    tailVisitedLocations.insert(knots[9])
 }
 
-tailVisitedLocations.insert(knots[9])
 print(tailVisitedLocations.count)
